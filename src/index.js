eae8debc0a29bcf5a12850546ac5ed7163df7148
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-02-03 12:26:33 -0800

    Added html,css, and js files, set up core html, border around game space, and comment block with pseudocode for js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/*
+~FN create input space: Number of rounds (add Name Later)
+~create tests for submission errors 
+~FN use input to initiate Game
+~FN startGame : for loop that calls "event" functions
+~~FN createTarget : creates a span?button element in a RANDOM LOCATION 
+that has click events
+~~Will eventually add randomness for size elements, and different shapes
+~~FN killTarget : called in click event, will call updateScore()
+~~FN updateScore : calls score tracker and increases Hits/Misses
+~~FN createSpace : make clickable background that calls updateScore()
+~FN Reset button which is called/created after for loop in startGame()
+*/
