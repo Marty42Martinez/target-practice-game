9b19195c8211a0bcd7c867b84570b35fa561b1d1
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-02-03 22:00:10 -0800

    create multiple buttons, that are killed on click

--- src/index.js
+++ src/index.js
@@ -11,3 +11,68 @@ that has click events
 ~~FN createSpace : make clickable background that calls updateScore()
 ~FN Reset button which is called/created after for loop in startGame()
 */
+
+const inputForm = document.getElementById('user-form');
+
+
+function createInputSpace(parentNode) {
+    const inputRounds = document.createElement('input');
+    const inputText = document.createElement('label');
+    const submitButton = document.createElement('button');
+    inputText.textContent = 'How many Rounds? ';
+    inputRounds.type = 'text';
+    inputRounds.placeholder = 'Number of Rounds';
+    submitButton.textContent = 'Submit';
+    submitButton.value = 'submit';
+    submitButton.classList.add('button');
+
+    
+    parentNode.appendChild(inputText);
+    parentNode.appendChild(inputRounds);
+    parentNode.appendChild(submitButton);
+    parentNode.addEventListener('submit', function(event) {
+        event.preventDefault();
+        startGame(inputRounds.value);
+
+    });
+}
+
+function startGame(numberRounds) {
+    const gameSpace = document.getElementById('game-space');
+    
+    for(let i = 0; i < numberRounds; i++) {
+        const xPos = parseInt(300*Math.random());
+        const yPos = parseInt(300*Math.random());
+        createTarget(xPos,yPos,gameSpace,i);
+    }
+}
+function createTarget(xPos,yPos,parent,index) {
+    const target = document.createElement('button');
+    const sizeChange = 40*(Math.random() + 0.5);
+    target.classList.add('target-button');
+    target.id = 'target'+ index;
+    target.style.left = xPos + 'px';
+    target.style.top = yPos + 'px';
+    target.style.height = sizeChange + 'px';
+    target.style.width = sizeChange + 'px';
+    
+    target.addEventListener('click', function() {
+        killTarget(target.id);
+    });
+    parent.appendChild(target);
+}
+
+
+function killTarget(target) {
+    const hitCounter = document.getElementById('hits');
+    removeElement(target);
+    hitCounter.textContent++;
+}
+
+function removeElement(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+}
+
+createInputSpace(inputForm);
